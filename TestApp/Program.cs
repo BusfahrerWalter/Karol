22ab52ddb2922bfcad4fbb8021e9188f62ccce0c
BusfahrerWalter <daniel.t.mueller@outlook.de>
2021-10-18 18:08:22 +0200

    sdf

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -14,21 +14,9 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            //for(int i = 0; i < 10; i++)
-            //{
-            //    Solve(i);
-            //}
-
-            World w1 = World.Load(@$"E:\Files\Schule\Schule 12\TrFi\C_Gartenzaun_Karol\labs\lab0.kdw");
-            w1.SetRenderingMode(WorldRenderingMode.Render2D);
-            Robot r1 = w1.Robots[0];
-
-            while (!r1.HasMark)
+            for (int i = 0; i < 10; i++)
             {
-                if (r1.HasWall || !r1.HasWallLeft)
-                    r1.TurnRight();
-                else
-                    r1.Move();
+                Solve(i);
             }
         }
 
